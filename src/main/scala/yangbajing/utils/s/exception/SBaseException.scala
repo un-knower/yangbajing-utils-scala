9b7c05d442b13708106124e362560868bce86d76
Yang Jing <yangbajing@gmail.com>
2016-02-26 10:34:21 +0800

    update Exception

--- src/main/scala/yangbajing/utils/s/exception/SBaseException.scala
+++ src/main/scala/yangbajing/utils/s/exception/SBaseException.scala
@@ -6,21 +6,30 @@ import yangbajing.utils.s.TMessage
   * 异常
   * Created by jingyang on 15/4/13.
   */
-abstract class SBaseException(val msg: String, val code: Int) extends RuntimeException(msg) with TMessage {
+abstract class SBaseException(val msg: String, val code: Int, val cause: Throwable) extends RuntimeException(msg, cause) with TMessage {
   override def toString = code + ": " + super.toString
 }
 
-case class SException(override val code: Int,
-                      override val msg: String) extends SBaseException(msg, code)
+//case class SException(override val code: Int,
+//                      override val msg: String,
+//                      override val cause: Throwable = null) extends SBaseException(msg, code, cause)
 
 case class SUnauthorizedException(override val msg: String = "Unauthorized",
-                                  override val code: Int = 401) extends SBaseException(msg, code)
+                                  override val code: Int = 401,
+                                  override val cause: Throwable = null) extends SBaseException(msg, code, cause)
 
 case class SBadException(override val msg: String = "Unauthorized",
-                         override val code: Int = 400) extends SBaseException(msg, code)
+                         override val code: Int = 400,
+                         override val cause: Throwable = null) extends SBaseException(msg, code, cause)
 
-case class SInternalErrorException(override val msg: String = "Internal Exception Error",
-                                   override val code: Int = 500) extends SBaseException(msg, code)
+case class SForbiddenException(override val msg: String,
+                                override val code: Int = 403,
+                                override val cause: Throwable = null) extends SBaseException(msg, code, cause)
 
 case class SNotFoundException(override val msg: String = "Not Found",
-                              override val code: Int = 404) extends SBaseException(msg, code)
+                              override val code: Int = 404,
+                              override val cause: Throwable = null) extends SBaseException(msg, code, cause)
+
+case class SInternalErrorException(override val msg: String = "Internal Exception Error",
+                                   override val code: Int = 500,
+                                   override val cause: Throwable = null) extends SBaseException(msg, code, cause)
